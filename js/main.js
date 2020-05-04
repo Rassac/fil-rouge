4c685b614378538602f699c76165b3db854fb968
Théo Cassar <theo.cassar@icloud.com>
2020-05-04 19:47:17 +0200

    jQuery

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
-  // On cache les sous-menus :
-  $(".sous").hide();
-  // On sélectionne tous les items de liste portant la classe "toggleSubMenu"
+      // On cache les sous - menus:
+      $("#sous").hide();
 
-  // On modifie l'évènement "click" sur les liens dans les items de liste
-  // qui portent la classe "toggleSubMenu" :
-  $("#prestations").click(function() {
+      // $("#prestations").click(function() {
+      //   $(.sous).show();
+      // });
 
-    // Si le sous-menu était déjà ouvert, on le referme :
-    if ($(this).next(".sous").length != 0) {
-      $(this).next(".sous").slideUp("normal");
-    }
 
-    // Si le sous-menu est caché, on ferme les autres et on l'affiche :
-    else {
-      $(".sous").slideUp("normal");
-      $(this).next(".sous").slideDown("normal");
-    }
-
-    // On empêche le navigateur de suivre le lien :
-    return false;
-  });
-});
+    };
