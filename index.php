9e763cfd11e887e688ab53845a61cd4fb4cefa8f
Théo Cassar <theo.cassar@icloud.com>
2020-05-07 15:30:42 +0200

    Nom des pages dans les onglets

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="La première entreprise de sécurisation des données informatique pour tous."/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title>Header</title>
+  <title>TCOMPUTING - ACCUEIL</title>
 </head>
 
 <body>
