83d68ac23e70a1a50a0e787579772b900aef00ae
Théo Cassar <theo.cassar@icloud.com>
2020-05-07 23:58:50 +0200

    Modification  de fichiers pour le menu burger

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
-$(document).ready(function() {
-  $('#sous').hide();
+$(document).ready(function () {
+  $("#sous").hide();
 
   var ouvert = false;
 
-  $('#prestations').click(function() {
+  $("#prestations").click(function () {
     if (ouvert) {
-      $('#sous').hide();
+      $("#sous").hide();
       ouvert = false;
     } else {
-      $('#sous').show();
+      $("#sous").show();
       ouvert = true;
     }
-    // console.log('ici');v
+    console.log("ici");
   });
+});
 
-//   function validateForm() {
-//     var name = document.forms["myForm"]["name"];
-//     var email = document.forms["myForm"]["email"];
-//     var message = document.forms["myForm"]["message"];
+// function validateForm() {
+//   var name = document.forms["myForm"]["name"];
+//   var email = document.forms["myForm"]["email"];
+//   var message = document.forms["myForm"]["message"];
 
-//     if (name.value == "") {
-//       document.getElementById('errorname').innerHTML = "Veuillez entrez un nom valide";
-//       name.focus();
-//       return false;
-//     } else {
-//       document.getElementById('errorname').innerHTML = "";
-//     }
-
-//     if (email.value == "") {
-//       document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
-//       email.focus();
-//       return false;
-//     } else {
-//       document.getElementById('erroremail').innerHTML = "";
-//     }
-
-//     if (email.value.indexOf("@", 0) < 0) {
-//       document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
-//       email.focus();
-//       return false;
-//     }
-
-//     if (email.value.indexOf(".", 0) < 0) {
-//       document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
-//       email.focus();
-//       return false;
-//     }
-
-//     if (message.value == "") {
-//       document.getElementById('errormsg').innerHTML = "Veuillez entrez un message valide";
-//       message.focus();
-//       return false;
-//     } else {
-//       document.getElementById('errormsg').innerHTML = "";
-//     }
-
-//     return true;
+//   if (name.value == "") {
+//     document.getElementById('errorname').innerHTML = "Veuillez entrez un nom valide";
+//     name.focus();
+//     return false;
+//   } else {
+//     document.getElementById('errorname').innerHTML = "";
 //   }
-// });
+
+//   if (email.value == "") {
+//     document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
+//     email.focus();
+//     return false;
+//   } else {
+//     document.getElementById('erroremail').innerHTML = "";
+//   }
+
+//   if (email.value.indexOf("@", 0) < 0) {
+//     document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
+//     email.focus();
+//     return false;
+//   }
+
+//   if (email.value.indexOf(".", 0) < 0) {
+//     document.getElementById('erroremail').innerHTML = "Veuillez entrez une adresse mail valide";
+//     email.focus();
+//     return false;
+//   }
+
+//   if (message.value == "") {
+//     document.getElementById('errormsg').innerHTML = "Veuillez entrez un message valide";
+//     message.focus();
+//     return false;
+//   } else {
+//     document.getElementById('errormsg').innerHTML = "";
+//   }
+
+//   return true;
+// }
