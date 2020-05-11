a0a65bff11c3d2c084de4544d4e669ab124a22cf
Théo Cassar <theo.cassar@gmail.com>
2020-05-11 22:51:24 +0200

    commit test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="shortcut icon" type="image/png" href="img/flavicon.png" />
-  <title>TCOMPUTING - ACCUEIL</title>
+  <title>TCOMPUTING – ACCUEIL</title>
 </head>
 
 <body>
